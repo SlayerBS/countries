99de048fa1bb450bb26ace68c49719eea2fb7a9e
Sergii Bogush <sergbogush@gmail.com>
2021-12-05 08:56:57 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,13 +8,29 @@ const HeaderEl = styled.header`
   background-color: (--colors-ui-base);
 `;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 0;
+`;
 
 const Title = styled.a.attrs({
   href: "/",
-})``;
+})`
+  color: var(--color-text);
+  font-size: var(--fs-sm);
+  text-decoration: none;
+  font-weight: var(--fw-bold);
+`;
 
-const ModeSwitcher = styled.div``;
+const ModeSwitcher = styled.div`
+  color: var(--color-text);
+  font-size: var(--fs-sm);
+  cursor: pointer;
+  font-weight: var(--fw-bold);
+  text-transform: capitalize;
+`;
 
 export const Header = () => {
   const [theme, setTheme] = useState("light");
@@ -30,8 +46,13 @@ export const Header = () => {
         <Wrapper>
           <Title>Where is the world?</Title>
           <ModeSwitcher onClick={toggleTheme}>
-            <IoMoon />
-            Light Theme
+            {theme === "light" ? (
+              <IoMoonOutline size="14px" />
+            ) : (
+              <IoMoon size="14px" />
+            )}
+
+            <span style={{ marginLeft: "0.75rem" }}>{theme} Theme</span>
           </ModeSwitcher>
         </Wrapper>
       </Container>
