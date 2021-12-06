c2ac68fc4cd880aec7dace33a66f06de8ddebb62
Sergii Bogush <sergbogush@gmail.com>
2021-12-06 10:55:11 +0200

    add Details

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -2,19 +2,29 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { IoArrowBack } from "react-icons/io5";
+import { searchByCountry } from "../config";
+import { Button } from "../components/Button";
+import { Info } from "../components/Info";
 
 export const Details = () => {
   const { name } = useParams();
   const navigate = useNavigate();
-
   const goBack = () => navigate(-1);
+  const [country, setCountry] = useState(null);
+
+  console.log(country);
+
+  useEffect(() => {
+    axios.get(searchByCountry(name)).then(({ data }) => setCountry(data[0]));
+  }, [name]);
 
   return (
     <div>
-      <button onClick={goBack}>
+      <Button onClick={goBack}>
         <IoArrowBack />
         Back
-      </button>
+      </Button>
+      {country && <Info navigate={navigate} {...country} />}
       Details {name}
     </div>
   );
