9a88e02c6bc4bf9aa4b0416f0672b236120a9bcd
Sergii Bogush <sergbogush@gmail.com>
2021-12-06 09:24:01 +0200

    Update Details.jsx

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,3 +1,21 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { IoArrowBack } from "react-icons/io5";
+
 export const Details = () => {
-  return <div>Details</div>;
+  const { name } = useParams();
+  const navigate = useNavigate();
+
+  const goBack = () => navigate(-1);
+
+  return (
+    <div>
+      <button onClick={goBack}>
+        <IoArrowBack />
+        Back
+      </button>
+      Details {name}
+    </div>
+  );
 };
